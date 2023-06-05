29654d0d3d5d208f65d8673a1ba868bc1654a661
Jazmine Miller <jazminedmiller@gmail.com>
2023-06-05 13:31:04 -0400

    add cocktaildb api to app, add function to handle search input

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,11 +1,36 @@
-import React from 'react'
+import React, { useEffect, useState }from 'react'
 import HeroImg from '../assets/hero-img.png'
 import BrowseHome from '../components/BrowseHome'
 import FavoritesHome from '../components/FavoritesHome'
 import Featured from '../components/Featured'
-
+import axios from 'axios'
 
 export default function Home() {
+    const [searchInput, setSearchInput] = useState('')
+    const [searchResults, setSearchResults] = useState([])
+
+    const handleSearchChange = (e) => {
+        setSearchInput(e.target.value)
+    }
+
+    const searchCocktails = async () => {
+        try {
+            const res = await axios.get(`https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${searchInput}`)
+            setSearchResults(res.data.drinks)
+            console.log(res)
+            
+        }
+        catch (error) {
+            console.error(`Error fetching results: ${error}`)
+        }
+    }
+
+    useEffect(() => {
+        if(searchInput){
+            searchCocktails()
+        }
+    }, [searchInput])
+
   return (
     <div>
         {/* ***** HERO CONTAINER ****** */}
@@ -17,7 +42,7 @@ export default function Home() {
                             Sip, Savor, and<p className='p-0 m-0 discover-text'>Discover:</p>Your Mixology<br /> Adventure Awaits
                     </div>
                     <div className='row pt-5'>
-                        <input type="text" placeholder="Search cocktail recipes"></input>
+                        <input type="text" placeholder="Search cocktail recipes" value={searchInput} onChange={handleSearchChange}></input>
                     </div>
                 </div>
                 <div className='order-md-last hero-img-col order-first col-sm-12 col-md-6 test-col'>
