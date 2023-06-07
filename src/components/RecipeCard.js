90c2b53195010e74203856be897c61506ba115b0
Jazmine Miller <jazminedmiller@gmail.com>
2023-06-07 13:00:22 -0400

    update card components for search results. add grid layout to results page

--- src/components/RecipeCard.js
+++ src/components/RecipeCard.js
@@ -1,60 +1,25 @@
 import React from "react";
-import Drink1 from '../assets/drink-img-1.jpg'
-import Drink2 from '../assets/drink-img-2.jpg'
-import Drink3 from '../assets/drink-img-3.jpg'
 
-function RecipeCard() {
+function RecipeCard({title, description, image, link}) {
   return (
     <div>
-      <div className=" pt-2 container">
-        <div className="row justify-content-center justify-content-md-between">
-          <div className="col-10 col-md-6 col-lg-4">
+      {/* <div className=" pt-2 container">
+        <div className="row justify-content-center justify-content-md-between"> */}
+          
         <div class="card">
-                <img src={Drink1} class="card-img-top" alt="..." />
+                <img src={image} class="card-img-top" alt={title} />
                 <div class="card-body">
-                  <h5 class="card-title">Card title</h5>
+                  <h5 class="card-title">{title}</h5>
                   <p class="card-text">
-                    Some quick example text to build on the card title and make
-                    up the bulk of the card's content.
+                    {description}
                   </p>
-                  <a href="#" class="btn btn-primary">
-                    Go somewhere
+                  <a href={link} class="btn btn-primary">
+                    View Recipe
                   </a>
                 </div>
               </div>
-              </div>
-              <div className="col-10 col-md-6 col-lg-4">
-        <div class="card">
-                <img src={Drink2} class="card-img-top" alt="..." />
-                <div class="card-body">
-                  <h5 class="card-title">Card title</h5>
-                  <p class="card-text">
-                    Some quick example text to build on the card title and make
-                    up the bulk of the card's content.
-                  </p>
-                  <a href="#" class="btn btn-primary">
-                    Go somewhere
-                  </a>
-                </div>
-              </div>
-              </div>
-              <div className="col-10 col-md-6 col-lg-4">
-        <div class="card">
-                <img src={Drink3} class="card-img-top" alt="..." />
-                <div class="card-body">
-                  <h5 class="card-title">Card title</h5>
-                  <p class="card-text">
-                    Some quick example text to build on the card title and make
-                    up the bulk of the card's content.
-                  </p>
-                  <a href="#" class="btn btn-primary">
-                    Go somewhere
-                  </a>
-                </div>
-              </div>
-              </div>
-        </div>
-      </div>
+        {/* </div>
+      </div> */}
     
     </div>
   );
--- src/pages/Results.js
+++ src/pages/Results.js
@@ -1,6 +1,7 @@
 import React, { useContext } from "react";
 import SearchBar from "../components/SearchBar";
 import SearchContext from "../components/SearchContext";
+import RecipeCard from "../components/RecipeCard";
 
 function Results() {
   const { searchResults } = useContext(SearchContext);
@@ -8,16 +9,23 @@ function Results() {
   return (
     <div>
         <SearchBar />
+        <div className="row mx-2 my-2">
       {searchResults &&
         searchResults.map(
           (drink, index) =>
-            drink && (
-              <div key={drink.idDrink || index}>
-                <h2>{drink.strDrink}</h2>
-                <img src={drink.strDrinkThumb} alt={drink.strDrink} />
-              </div>
+            (
+            <div className="my-2 col-6 col-md-4 col-lg-3">
+             <RecipeCard 
+             key={index}
+             title={drink.strDrink}
+             description={drink.strInstructions}
+             image={drink.strDrinkThumb}
+             link={`/recipe/${drink.idDrink}`}
+             />
+            </div>
             )
         )}
+        </div>
     </div>
   );
 }
