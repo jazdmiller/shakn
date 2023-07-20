6a2155fd0d6e466ce758e777d6c546ad806b349f
Jazmine Miller <jazminedmiller@gmail.com>
2023-07-20 13:35:51 -0400

    style blog cards on blog page with bootstrap. overlay text over background.

--- client/src/pages/Blog.js
+++ client/src/pages/Blog.js
@@ -24,15 +24,32 @@ function Blog() {
 
   return (
     <div>
-        
+        <div class="container">
+            <h1 class="text-center my-5">Shakn Blog</h1>
         {loading ? <p>Loading...</p> :
             articles.map((article, index) => (
-            <div key={index}>
-                <h2>{article.attributes.title}</h2>
-                <img src={`http://localhost:1337${article.attributes.image.data.attributes.url}`} alt={article.title} />
-                <p>{article.attributes.content}</p>
+            <div key={index} class="card w-75 mx-auto bg-dark text-white">
+                <img class="card-img" src={`http://localhost:1337${article.attributes.image.data.attributes.url}`} alt={article.attributes.title}></img>
+                <div class="card-img-overlay">
+                    <h5>{article.attributes.title}</h5>
+                    <p class="card-text w-75">{article.attributes.content}</p>
+                    <a>Read More</a>
+                </div>
+                {/* <div class="card-body">
+                    <h5 class="card-title">{article.attributes.title}</h5>
+                    <div class="row">
+                        <div class="col-10">
+                            <p class="card-text">{article.attributes.content}</p>
+                        </div>
+                        <div class="col-2">
+                            <button>Read More</button>
+                        </div>
+                    </div>
+                </div>
+                <img class="card-img-bottom" src={`http://localhost:1337${article.attributes.image.data.attributes.url}`} alt={article.title} /> */}
             </div>
         ))}
+        </div>
     </div>
   )
 }
