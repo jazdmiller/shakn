9b2f1b6e6dd365ef046de39a68f960458cca7e3b
Jazmine Miller <jazminedmiller@gmail.com>
2023-08-03 14:17:52 -0400

    create card component for blog section on home page

--- client/src/components/BlogCard.js
+++ client/src/components/BlogCard.js
@@ -1,17 +1,20 @@
 import React from 'react'
-import Drink1 from '../assets/drink-img-1.jpg'
 
-export default function BlogCard() {
+
+export default function BlogCard({ article }) {
   return (
     <div>
-        <div class="card text-bg-dark">
-  <img src={Drink1} class="card-img" alt="..." />
-  <div class="card-img-overlay">
-    <h5 class="card-title">Card title</h5>
-    <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-    <p class="card-text"><small>Last updated 3 mins ago</small></p>
-  </div>
-</div>
+        <div class="card blog-card">
+          <img src={`http://localhost:1337${article.image}`} alt={article.title}></img>
+          <div class="card-body">
+            <div>
+              <h5 class="card-title">{article.title}</h5>
+              <p class="card-text">{article.content}</p>
+              <Link to={`/article/${article.id}`} >Read Article</Link>
+            </div>
+
+          </div>
+        </div>
     </div>
   )
 }
