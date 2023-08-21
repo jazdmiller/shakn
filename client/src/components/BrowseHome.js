c19043a00b12dd96011bcbc42aea61ddf27e1b9f
Jazmine Miller <jazminedmiller@gmail.com>
2023-08-21 14:22:12 -0400

    add stagger animation to browse labels (homepage)

--- client/src/components/BrowseHome.js
+++ client/src/components/BrowseHome.js
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import CategoryLabel from "./CategoryLabel";
 import { useNavigate, Link } from "react-router-dom";
+import { motion } from 'framer-motion'
 
 export const categories = [
   "Tequila",
@@ -16,6 +17,38 @@ function BrowseHome() {
 
   const navigate= useNavigate()
 
+  const container ={
+    hidden: {opacity: 0},
+    visible: (i = 1) => ({
+      opacity: 1,
+      transition: {
+        staggerChildren: 0.05,
+        delayChildren: 0.02 * i
+      }
+    })
+  }
+  const staggerLabels = {
+    hidden: {
+      opacity: 0,
+      y: -20,
+      transition: {
+        type:"spring",
+        damping: 20,
+        stiffness: 100
+      }
+    },
+
+    visible: {
+      opacity: 1,
+      y: 0,
+      transition: {
+        type: "spring",
+        damping: 20,
+        stiffness: 100
+      }
+    }
+  }
+
   const handleClick = () => {
     navigate(`/recipe/random`)
   }
@@ -29,21 +62,21 @@ function BrowseHome() {
             </div>
         <div className="row my-3">
         </div>
-        <div className="row mx-2 mt-2">
+        <motion.div variants={container} initial="hidden" whileInView="visible" viewport={{once : true}}  className="row mx-2 mt-2">
           {categories.map((category) => (
             
-            <div className=" my-2 col-6 col-md-4 col-lg-3 ">
+            <motion.div variants={staggerLabels} className=" my-2 col-6 col-md-4 col-lg-3 ">
               <Link to={`/results/${category}`} key={category}>
             <CategoryLabel category={category} />
             </Link>
-            </div>
+            </motion.div>
           ))}
-          <div className="my-2 col-6 col-md-4 col-lg-3" >
+          <motion.div variants={staggerLabels} className="my-2 col-6 col-md-4 col-lg-3" >
             <Link to={'/recipe/random'}>
             <CategoryLabel id="random-btn" category='Random Drink' onClick={handleClick} />
             </Link>
-          </div>
-        </div>
+          </motion.div>
+        </motion.div>
       </div>
     </div>
   );
