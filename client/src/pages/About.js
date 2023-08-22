77ffc414937c2b92b0781561ae3c590c07760011
Jazmine Miller <jazminedmiller@gmail.com>
2023-08-22 13:20:01 -0400

    update desktop layout - about page

--- client/src/pages/About.js
+++ client/src/pages/About.js
@@ -7,24 +7,24 @@ function About() {
     <div>
       <div className="container-fluid about-container">
         <div className="row p-5">
-          <div className="col-12 order-first order-md-last col-md-6 about-img text-center mt-5">
+          <div className="col-12 order-first order-md-last about-img text-center mt-0">
             <img
               src={AboutImg}
               alt="cheers drink graphic"
               className="text-center"
             />
           </div>
-          <div className="col-12 col-md-6 p-5">
+          <div className="col-12 p-2 px-md-5 mt-3">
             <div className="row about-row mb-4">
             <span className="about-title">About Us</span>
             </div>
-            <p className="about-text">Shakn is an online platform dedicated to empowering
+            <p className="about-text mx-auto mt-5">Shakn is an online platform dedicated to empowering
             mixology enthusiasts worldwide with an extensive collection of
             cocktail recipes. We aim to make home bartending an enjoyable and
             straightforward experience for both beginners and seasoned
             aficionados alike.</p> 
             
-            <p className="about-text">Our platform is a celebration of the art of
+            <p className="about-text mx-auto mt-4">Our platform is a celebration of the art of
             mixology, blending tradition with innovation to bring you a
             comprehensive catalogue of cocktail recipes. From classic
             concoctions such as the Old Fashioned and Margarita, to contemporary
