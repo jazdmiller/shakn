e3e5259ae2d7a6d222fc80bf8913004d74ddaab5
Jazmine Miller <jazminedmiller@gmail.com>
2023-08-27 22:39:16 -0400

    add hashrouter

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,6 @@
 
 import './App.css';
-import {BrowserRouter as Router, Route, Routes, useLocation} from 'react-router-dom'
+import {HashRouter as Router, Route, Routes, useLocation} from 'react-router-dom'
 import Navbar from './components/Navbar';
 import Home from './pages/Home';
 import Search from './pages/Search'
